840cefb95e48d0768aad6c2456feefb72243b474
Ajay Choudhary <ajaykc20307@gmail.com>
2024-06-12 00:07:28 +0530

    shift mongodb to online server and use of .env

--- db.js
+++ db.js
@@ -1,6 +1,10 @@
 const mongoose = require("mongoose");
-const mongoURL = "mongodb://127.0.0.1:27017/hotels";
+require("dotenv").config();
 
+// const mongoURL = "mongodb://127.0.0.1:27017/hotels";
+// const mongoURL = "mongodb+srv://imajay204:ajay@cluster0.heohtq6.mongodb.net/hoteldb";
+const local_url=process.env.LOCAL_URL
+mongoURL=process.env.DB_URL||local_url;
 mongoose.connect(mongoURL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -10,8 +14,7 @@ const db = mongoose.connection;
 
 db.on("connected", () => {
   console.log("connected to mongoDB");
-});
-db.on("error", (err) =>{
+});db.on("error", (err) =>{
     console.log("Mongodb connection error",err);
 })
 db.on("disconnected", () => {
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const db = require("./db");
 const Menu = require("./models/menu");
+require("dotenv").config();
 const bodyParser = require("body-parser");
 app.use(bodyParser.json());
 app.get("/", (req, res) => {
@@ -13,6 +14,8 @@ app.use('/person',personRoutes);
 
 const menuRoutes=require("./routes/menuRoutes.js");
 app.use('/menu',menuRoutes);
-app.listen(3000, () => {
-  console.log("server is running on port 3000");
+
+PORT=process.env.PORT ||3000
+app.listen(PORT, () => {
+  console.log("server is running on port",PORT);
 });
